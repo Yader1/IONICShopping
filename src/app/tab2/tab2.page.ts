81d99d150b7a8b9b466381202328af4d0d3149cf
Yader Campbell <yadermejiac123@gmail.com>
2021-04-09 11:03:01 -1200

    la pagina de añadir esta listo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ShoppingItemService } from '../services/shopping-item.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,70 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public item: string;
+
+  constructor(
+    private shoppingService: ShoppingItemService,
+    private alercontroller: AlertController
+  ) {}
+
+  addItem(){
+    console.log(this.item);
+
+    if(this.item &&  this.item != ''){
+      if(!this.shoppingService.existItem(this.item)){
+        this.shoppingService.addItem(this.item);
+        this.item='';
+        console.log(this.shoppingService.items);
+
+        this.AlertExistoso();
+      }else{
+        //mensajede cuando ya existe el elemento
+        this.AlertErrorExiste();
+      }
+    }else{
+        //mensaje de error cuando pasemos un valor vacio
+        this.AlertError();
+    }
+  }
+
+  async AlertExistoso() {
+    const alert = await this.alercontroller.create({
+      header: 'Exito',
+      message: 'Item añadido correctamente.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async AlertErrorExiste() {
+    const alert = await this.alercontroller.create({
+      header: 'ERROR',
+      message: 'Item ya existe.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async AlertError() {
+    const alert = await this.alercontroller.create({
+      header: 'ERROR',
+      message: 'Error, debes de escribir un item.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 
 }
