fefcee06d78ffe23b1cf65ae9ce912f209d894fc
Yader Campbell <yadermejiac123@gmail.com>
2021-05-19 01:47:07 -0600

    Se ha añadido la pantalla de lista de item

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ShoppingItemService } from '../services/shopping-item.service';
+
+import { AlertController, MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,59 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    public shoppingService: ShoppingItemService,
+    private alertController: AlertController,
+    private menuController: MenuController
+  ) {}
+  async removeItem(item: string){
+    const alert = await this.alertController.create({
+      header: 'Confirmacion',
+      message: '¿Seguro que quieres eliminar el elemento?',
+      buttons: [
+        {
+          text: 'Si',
+          handler: (blah) => {
+            this.shoppingService.removeItem(item);
+          }
+        }, {
+          text: 'No',
+          handler: () => {
+            alert. dismiss();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 
+  onRenderItems($event){
+    console.log($event);
+    const item = this.shoppingService.items.splice($event.detail.from, 1)[0];
+    this.shoppingService.items.splice($event.detail.to, 0, item);
+    $event.detail.complete();
+    console.log(this.shoppingService.items);
+  }
+  async removeAll(){
+    const alert = await this.alertController.create({
+      header: 'Confirmacion',
+      message: '¿Seguro que quieres eliminar el elemento?',
+      buttons: [
+        {
+          text: 'Si',
+          handler: (blah) => {
+            this.shoppingService.removeAllItems();
+            this.menuController.close()
+          }
+        }, {
+          text: 'No',
+          handler: () => {
+            alert. dismiss();
+            this.menuController.close()
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
